737cab72800319c4eeb61b3cdafb03d681e2d5d8
sigdeldebish <sigdel.bish@gmail.com>
2024-11-21 20:44:59 -0500

    Server Start Success

--- app/main.py
+++ app/main.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
 	if __name__ == "__main__":
 		import uvicorn
 
-		uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+		uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
